4a759beef499181cae15c1e8d4cd8902fc2d4823
Heather <224328814+HeatherR206@users.noreply.github.com>
2025-09-10 15:53:22 -0700

    feat and refactor: add for_of loop to execute code visible to user; delete ' ' from height key-values, define all types key-values as arrays, rename PokemonList variable

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,45 @@
-let PokemonList = [
+let pokemon = [
   {
     name: 'Bulbasaur',
-    height: '2',
+    height: 2,
+    abilities: ['overgrow'],
     types: ['grass', 'poison'] 
   },
 
   {
     name: 'Charmander',
-    height: '2',
-    types: 'fire'
+    height: 2,
+    abilities: ['blaze'],
+    types: ['fire']
   },
 
   {
     name: 'Squirtle',
-    height: '1.7',
-    types: 'water'
+    height: 1.7,
+    abilities: ['torrent'],
+    types: ['water']
   },
 
   {
     name: 'Gyarados',
-    height: '21',
+    height: 21.3,
+    abilities: ['intimidate'],
     types: ['water', 'flying']
+  },
+
+  { name: 'Eevee',
+    height: 1,
+    abilities: ['run away', 'adaptability'],
+    types: ['normal']
   }
 ];
+
+for (let i = 0; i < pokemon.length; i++) { 
+  //height value greater than 20 designates a special Pokemon
+  if (pokemon[i].height > 20) { 
+  //<p> used to assign each Pokemon it's own line
+  document.write('<p>',pokemon[i].name + ' (height: ' + pokemon[i].height,')' + ' - Wow, that\'s big!</p>');  
+  } else {
+  document.write('<p>',pokemon[i].name + ' (height: ' + pokemon[i].height,')</p>');
+  } 
+}
