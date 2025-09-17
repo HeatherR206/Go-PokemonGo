ba037142450a5f79d83ca799c9eb2592058f8ef0
Heather <224328814+HeatherR206@users.noreply.github.com>
2025-09-16 22:07:02 -0700

    refactor: wrap pokemonList array in IIFE to protect global state
    -create new pokemonRepository variable to hold IIFE return code
    -assign IIFE to new pokemonRepository variable
    - write IIFE with 'getAll' and 'add' return keys
    -update forEach() code to cope with changes to pokemonList in order to retrieve pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,60 @@
-let pokemonList= [
-  {
-    name: 'Bulbasaur',
-    height: 2,
-    abilities: ['overgrow'],
-    types: ['grass', 'poison'] 
-  },
+let pokemonRepository = (function () { 
+      
+  let pokemonList= [
+    {
+      name: 'Bulbasaur',
+      height: 2,
+      abilities: ['overgrow'],
+      types: ['grass', 'poison'] 
+    },
 
-  {
-    name: 'Charmander',
-    height: 2,
-    abilities: ['blaze'],
-    types: ['fire']
-  },
+    {
+      name: 'Charmander',
+      height: 2,
+      abilities: ['blaze'],
+      types: ['fire']
+    },
 
-  {
-    name: 'Squirtle',
-    height: 1.7,
-    abilities: ['torrent'],
-    types: ['water']
-  },
+    {
+      name: 'Squirtle',
+      height: 1.7,
+      abilities: ['torrent'],
+      types: ['water']
+    },
 
-  {
-    name: 'Gyarados',
-    height: 21.3,
-    abilities: ['intimidate'],
-    types: ['water', 'flying']
-  },
+    {
+      name: 'Gyarados',
+      height: 21.3,
+      abilities: ['intimidate'],
+      types: ['water', 'flying']
+    },
 
-  { name: 'Eevee',
-    height: 1,
-    abilities: ['run away', 'adaptability'],
-    types: ['normal']
+    { name: 'Eevee',
+      height: 1,
+      abilities: ['run away', 'adaptability'],
+      types: ['normal']
+    }
+  ];
+  
+  function add(pokemon) {
+      pokemonList.push(pokemon);
   }
-];
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+
+})(); 
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({ name: 'Pikachu' });
+
+let allPokemon = pokemonRepository.getAll();
+
 function listPokemonAndProperties(pokemon) {
   pokemon.forEach(pokemon => {
     const pokemonProperties = Object.entries(pokemon);
@@ -43,4 +65,4 @@ function listPokemonAndProperties(pokemon) {
     console.log('---------------------');
   });
 }
-listPokemonAndProperties(pokemonList);
+listPokemonAndProperties(allPokemon)
