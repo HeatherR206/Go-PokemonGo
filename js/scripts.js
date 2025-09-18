12d21beb2efbfdf060eec5edf3a207a40ca3649e
Heather <224328814+HeatherR206@users.noreply.github.com>
2025-09-17 21:57:00 -0700

    fix and refactor: refactor code to account for functions called within IIFE
    -new: "function add(pokemon)" and "function getAll( )"
    -function add(pokemon) used for Pikachu with key-value pairs as other Pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,65 @@
-let pokemonList = [
-  {
-    name: 'Bulbasaur',
-    height: 2,
-    abilities: ['overgrow'],
-    types: ['grass', 'poison'] 
-  },
+let pokemonRepository = (function () { 
+      
+  let pokemonList= [
+    {
+      name: 'Bulbasaur',
+      height: 2,
+      abilities: ['overgrow'],
+      types: ['grass', 'poison'] 
+    },
 
-  {
-    name: 'Charmander',
-    height: 2,
-    abilities: ['blaze'],
-    types: ['fire']
-  },
+    {
+      name: 'Charmander',
+      height: 2,
+      abilities: ['blaze'],
+      types: ['fire']
+    },
 
-  {
-    name: 'Squirtle',
-    height: 1.7,
-    abilities: ['torrent'],
-    types: ['water']
-  },
+    {
+      name: 'Squirtle',
+      height: 1.7,
+      abilities: ['torrent'],
+      types: ['water']
+    },
 
-  {
-    name: 'Gyarados',
-    height: 21.3,
-    abilities: ['intimidate'],
-    types: ['water', 'flying']
-  },
+    {
+      name: 'Gyarados',
+      height: 21.3,
+      abilities: ['intimidate'],
+      types: ['water', 'flying']
+    },
 
-  { name: 'Eevee',
-    height: 1,
-    abilities: ['run away', 'adaptability'],
-    types: ['normal']
+    { name: 'Eevee',
+      height: 1,
+      abilities: ['run away', 'adaptability'],
+      types: ['normal'],
+    }
+  ];
+  
+  function add(pokemon) {
+      pokemonList.push(pokemon);
   }
-];
 
-pokemonList.forEach(function (pokemon){
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+    
+})();
+console.log(pokemonRepository.getAll());
+
+pokemonRepository.add({ 
+  name: 'Pikachu',
+  height: 1.3,
+  abilities: ['static'],
+  types: ['electric'],
+});
+
+pokemonRepository.getAll().forEach(function (pokemon) {
   //height value greater than 20 designates a special Pokemon
   if (pokemon.height > 20) {
   //<p> used to assign each Pokemon it's own line  
