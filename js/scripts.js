69e7141a5228e1c5507e52064f353aa7f8e29c7e
Heather <224328814+HeatherR206@users.noreply.github.com>
2025-09-17 21:13:27 -0700

    fix: previous JavaScript using .forEach( ) did not return anything to browser
    -refactored old .forEach( ) so new .forEach( ) utilizes original for__of loop code.
    -return for new .forEach( ) code is the same as the for__of loop code

--- js/scripts.js
+++ js/scripts.js
@@ -1,68 +1,45 @@
-let pokemonRepository = (function () { 
-      
-  let pokemonList= [
-    {
-      name: 'Bulbasaur',
-      height: 2,
-      abilities: ['overgrow'],
-      types: ['grass', 'poison'] 
-    },
+let pokemonList = [
+  {
+    name: 'Bulbasaur',
+    height: 2,
+    abilities: ['overgrow'],
+    types: ['grass', 'poison'] 
+  },
 
-    {
-      name: 'Charmander',
-      height: 2,
-      abilities: ['blaze'],
-      types: ['fire']
-    },
+  {
+    name: 'Charmander',
+    height: 2,
+    abilities: ['blaze'],
+    types: ['fire']
+  },
 
-    {
-      name: 'Squirtle',
-      height: 1.7,
-      abilities: ['torrent'],
-      types: ['water']
-    },
+  {
+    name: 'Squirtle',
+    height: 1.7,
+    abilities: ['torrent'],
+    types: ['water']
+  },
 
-    {
-      name: 'Gyarados',
-      height: 21.3,
-      abilities: ['intimidate'],
-      types: ['water', 'flying']
-    },
+  {
+    name: 'Gyarados',
+    height: 21.3,
+    abilities: ['intimidate'],
+    types: ['water', 'flying']
+  },
 
-    { name: 'Eevee',
-      height: 1,
-      abilities: ['run away', 'adaptability'],
-      types: ['normal']
-    }
-  ];
-  
-  function add(pokemon) {
-      pokemonList.push(pokemon);
+  { name: 'Eevee',
+    height: 1,
+    abilities: ['run away', 'adaptability'],
+    types: ['normal']
   }
+];
 
-  function getAll() {
-    return pokemonList;
-  }
-
-  return {
-    add: add,
-    getAll: getAll
-  };
-
-})(); 
-console.log(pokemonRepository.getAll());
-pokemonRepository.add({ name: 'Pikachu' });
-
-let allPokemon = pokemonRepository.getAll();
-
-function listPokemonAndProperties(pokemon) {
-  pokemon.forEach(pokemon => {
-    const pokemonProperties = Object.entries(pokemon);
-
-    pokemonProperties.forEach(([key, value]) => {
-      console.log(`${key}: ${value}`);
-    });
-    console.log('---------------------');
-  });
-}
-listPokemonAndProperties(allPokemon)
+pokemonList.forEach(function (pokemon){
+  //height value greater than 20 designates a special Pokemon
+  if (pokemon.height > 20) {
+  //<p> used to assign each Pokemon it's own line  
+  document.write('<p>',pokemon.name + ' (height: ' + pokemon.height,')' + ' - Wow, that\'s big!</p>');  
+  } else {
+  document.write('<p>',pokemon.name + ' (height: ' + pokemon.height,')</p>');
+  }  
+});
