baea37087aaa954ef081c20e681971bc85ff1c3e
Heather <224328814+HeatherR206@users.noreply.github.com>
2025-09-24 18:30:56 -0700

    feat: update scripts.js file using AJAX to send and fetch data asynchronously from an external Pokemon API (pokeapi)

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,36 @@
-let pokemonRepository = (function () {  // IIFE function block
+let pokemonRepository = (function () { // IIFE function block
       
-  let pokemonList= [
-    {
-      name: 'Bulbasaur',
-      height: 2,
-      abilities: ['overgrow'],
-      types: ['grass', 'poison'] 
-    },
-
-    {
-      name: 'Charmander',
-      height: 2,
-      abilities: ['blaze'],
-      types: ['fire']
-    },
-
-    {
-      name: 'Squirtle',
-      height: 1.7,
-      abilities: ['torrent'],
-      types: ['water']
-    },
-
-    {
-      name: 'Gyarados',
-      height: 21.3,
-      abilities: ['intimidate'],
-      types: ['water', 'flying']
-    },
-
-    { name: 'Eevee',
-      height: 1,
-      abilities: ['run away', 'adaptability'],
-      types: ['normal'],
-    }
-  ];
+  let pokemonList= [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
   
+  function showLoadingMessage() { // Function to show the loading message
+    let loadingMessage = document.createElement('div');
+    loadingMessage.id = 'loading-message';
+    loadingMessage.innerText = 'Loading...';
+    document.body.appendChild(loadingMessage);
+  }
+
+  function hideLoadingMessage() { // Function to hide the loading message
+    let loadingMessage = document.querySelector('#loading-message');
+    if (loadingMessage) {
+      loadingMessage.remove();
+    }
+  }
+
   function add(pokemon) {
+    if (
+      typeof pokemon === "object" &&
+      "name" in pokemon &&
+      "detailsUrl" in pokemon
+    ) {
       pokemonList.push(pokemon);
+    } else {
+      console.log("pokemon is not correct");
+    }
   }
 
   function getAll() {
     return pokemonList;
-  }
-  function showDetails(pokemon) {
-    console.log(pokemon);
   }
 
   function addListItem(pokemon) { 
@@ -55,31 +41,65 @@ let pokemonRepository = (function () {  // IIFE function block
     button.classList.add('button-class'); // add a class 'pokemon-name' to the button 
     listPokemon.appendChild(button); // append the button to list item
     pokemonList.appendChild(listPokemon); // append the list item to the (<ul>) in HTML
-    button.addEventListener('click', function () { // generate Pokemon object keys when button is clicked
+    button.addEventListener('click', function() { //generate Pokemon object keys when button is clicked
       showDetails(pokemon);
     });
-  }  
+  }
+
+  function loadList() {
+    showLoadingMessage(); // Show message at the start of fetch
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function(json) {
+      json.results.forEach(function(item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+      });
+      hideLoadingMessage(); // Hide message on successful fetch
+    }).catch(function(e) {
+      hideLoadingMessage(); // Hide message on error
+      console.error('Error fetching list: ', e);
+    })
+  }
+
+  function loadDetails(item) {
+    showLoadingMessage(); // Show message at the start of fetch
+    let url = item.detailsUrl;
+    return fetch(url).then(function(response) {
+      return response.json();
+    }).then(function(details) { // Now we add the details to the item  
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = details.types;
+      hideLoadingMessage(); // Hide message on successful fetch
+    }).catch(function(e) {
+      console.error('Error fetching details: ', e);
+      hideLoadingMessage(); // Hide message on error
+    });
+  }
+
+  function showDetails(pokemon) {
+    loadDetails(pokemon).then(function () {
+    console.log(pokemon);
+    });
+  }
 
   return {
     add: add,
     getAll: getAll,
-    addListItem: addListItem
+    addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails,
+    showDetails: showDetails
   };
     
-})();
+})(); 
 
-console.log(pokemonRepository.getAll());
-
-pokemonRepository.add({ 
-  name: 'Pikachu',
-  height: 1.3,
-  abilities: ['static'],
-  types: ['electric'],
-
-});
-
-console.log(pokemonRepository.getAll());  
-
-pokemonRepository.getAll().forEach(function (pokemon) { 
-  pokemonRepository.addListItem(pokemon); // IIFE variable calls 'addListItem' fn
+pokemonRepository.loadList().then(function() { // Now the data is loaded!
+  pokemonRepository.getAll().forEach(function (pokemon) { 
+    pokemonRepository.addListItem(pokemon); // IIFE variable calls 'addListItem' fn
+  }); 
 });
